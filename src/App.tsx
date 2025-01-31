cbf6776760a5e63434ab3f0323106d333aeb44a9
= <=>
2025-01-31 09:43:17 +0800

    problem: can't test dgraph endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://green-feather-41451536.ap-south-1.aws.cloud.dgraph.io/graphql/",
+  cache: new InMemoryCache(),
+  headers: {
+    "X-Auth-Token": "M2ViNGRlYzJhZDJkNDQ2MTc1YTA5ODJkOGJiNmQ5ZWQ="
+  }
+});
+
+function query() {
+  client
+    .query({
+      query: gql`
+        queryUser {
+          OID
+          github
+          name
+          telegram
+          rate
+        }
+      `,
+    })
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error));
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -21,6 +53,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={query}>Query</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +62,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
