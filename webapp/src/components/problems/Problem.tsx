d61fd6c9af8c3c6981dcc02b75b7f49fd855a64a
gsovereignty <gsovereignty@proton.me>
2025-02-08 17:03:01 +0800

    problem: relevant problem data is not displayed on /problems/$id

--- webapp/src/components/problems/Problem.tsx
+++ webapp/src/components/problems/Problem.tsx
@@ -1,7 +1,19 @@
+import { useProblemQuery } from './types/problem.gen';
+
 type ProblemProps = {
   id: string;
 };
 
 export default function Problem({ id }: ProblemProps) {
-  return <h1>{id}</h1>;
+  const { data, loading, error } = useProblemQuery({ variables: { id } });
+
+  if (loading) return <div>Loading...</div>;
+  if (error || !data?.getProblem?.id) return <div>Error loading problem.</div>;
+
+  return (
+    <div>
+      <h1 className="text-3xl">{data.getProblem?.title}</h1>
+      <pre>{JSON.stringify(data.getProblem, null, 2)}</pre>
+    </div>
+  );
 }
